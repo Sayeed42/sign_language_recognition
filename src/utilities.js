8dd93f85af8bc60f4806fc3722379fbc4e79ad6d
anmukhop <77803556+anmukhop@users.noreply.github.com>
2023-03-30 16:36:16 -0400

    Update utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -8,32 +8,9 @@ const fingerJoints = {
 };
 
 // Infinity Gauntlet Style
-const style = {
-  0: { color: "yellow", size: 15 },
-  1: { color: "gold", size: 6 },
-  2: { color: "green", size: 10 },
-  3: { color: "gold", size: 6 },
-  4: { color: "gold", size: 6 },
-  5: { color: "purple", size: 10 },
-  6: { color: "gold", size: 6 },
-  7: { color: "gold", size: 6 },
-  8: { color: "gold", size: 6 },
-  9: { color: "blue", size: 10 },
-  10: { color: "gold", size: 6 },
-  11: { color: "gold", size: 6 },
-  12: { color: "gold", size: 6 },
-  13: { color: "red", size: 10 },
-  14: { color: "gold", size: 6 },
-  15: { color: "gold", size: 6 },
-  16: { color: "gold", size: 6 },
-  17: { color: "orange", size: 10 },
-  18: { color: "gold", size: 6 },
-  19: { color: "gold", size: 6 },
-  20: { color: "gold", size: 6 },
-};
 
 // Drawing function
-export const drawHand = (predictions, ctx) => {
+export const drawHand = (predictions, ctx, style) => {
   // Check if we have predictions
   if (predictions.length > 0) {
     // Loop through each prediction
