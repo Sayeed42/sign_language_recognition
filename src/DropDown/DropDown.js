673faa2f0c0cdfc73baae33b58b70dc728938178
Md Sayeedul Islam <mdsayeedul@vt.edu>
2023-05-02 13:21:12 -0400

    Fixed Dropdown

--- src/DropDown/DropDown.js
+++ src/DropDown/DropDown.js
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { poseImages } from '../pose_images'
 
 import './DropDown.css'
 
 export default function DropDown({ poseList, currentPose, setCurrentPose }) {
-return (
-        <div
-        className='dropdown dropdown-container'
-         
-      >
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => setIsOpen(!isOpen);
+  
+    const handleSelect = (pose) => {
+      setIsOpen(false);
+      setCurrentPose(pose);
+    };
+  
+    return (
+      <div className='dropdown dropdown-container'>
         <button 
-            className="btn btn-secondary dropdown-toggle"
-            type='button'
-            data-bs-toggle="dropdown"
-            id="pose-dropdown-btn"
-            aria-expanded="false"
-        >{currentPose}
+          className="btn btn-secondary dropdown-toggle"
+          type='button'
+          onClick={handleToggle}
+        >
+          {currentPose}
         </button>
-        <ul class="dropdown-menu dropdown-custom-menu" aria-labelledby="dropdownMenuButton1">
-            {poseList.map((pose) => (
-                <li onClick={() => setCurrentPose(pose)}>
-                    <div class="dropdown-item-container">
-                        <p className="dropdown-item-1">{pose}</p>
-                        <img 
-                            src={poseImages[pose]}
-                            className="dropdown-img"
-                        />
-                        
-                    </div>
-                </li>
-            ))}
-            
-        </ul>
-              
-          
+        <ul 
+          className={`dropdown-menu dropdown-custom-menu ${isOpen ? 'show' : ''}`}
+          aria-labelledby="pose-dropdown-btn"
+        >
+          {poseList.map((pose) => (
+            <li 
+              key={pose} 
+              onClick={() => handleSelect(pose)}
+            >
+              <div className="dropdown-item-container">
+                <p className="dropdown-item-1">{pose}</p>
+                <img 
+                  src={poseImages[pose]}
+                  className="dropdown-img"
+                />
+              </div>
+            </li>
+          ))}
+        </ul> 
       </div>
-    )
-}
+    );
+  }
  
