3cf808a99014e9907a70d05b67f5af7ace2fe36d
Md Sayeedul Islam <mdsayeedul@vt.edu>
2023-05-02 15:29:59 -0400

    Updated information, grouped dropdown

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Sign Language Recognition
 ## Using Demo Site
 https://sayeed42.github.io/sign_language_recognition/
+1. When App ask for permission of camera, allow it access to capture hand gesture.
+
+2. Select what sign you want to do from the dropdown menu.
+
+3. Click on Start pose and see the image of the that pose in the right side and replicate that image in front of camera.
+
+4. If you do that correctly the points over the video will become green in color.
 ## Running from Repository
 After `git clone` run
 ```
--- src/Information/Information.js
+++ src/Information/Information.js
@@ -8,7 +8,16 @@ export default function Information() {
             <h1 className="info-heading">Why is it important?</h1>
             <div className="info-main">
                 <p className="info-content">
-                    
+                American Sign Language (ASL) is a visual language used by the Deaf community in the United States and parts of Canada. It is a fully developed language with its own grammar, syntax, and vocabulary. ASL uses a combination of handshapes, facial expressions, and body language to convey meaning.
+                </p>
+                <p className="info-content">
+                Learning sign language has many benefits, including improving communication skills, promoting inclusion and understanding of the Deaf community, and enhancing cognitive and motor skills. Additionally, learning sign language can provide career opportunities in fields such as education, interpretation, and social work.
+                </p>
+                <p className="info-content">
+                It is also important to recognize the impact of language deprivation on the Deaf community. Many Deaf individuals do not have access to language in their early years, which can lead to delays in cognitive and linguistic development. Learning and using sign language can help prevent language deprivation and promote language acquisition for Deaf individuals.
+                </p>
+                <p className="info-content">
+                One of the key challenges of learning and understanding sign language is the difference among some groups of signs can be very small and subtle. So it can be difficult to detect a particular sign. This system tries to group similar and confusing signs and tries to convey the subtle differences among them to the user. At the backend of this system, the AI model captures the differences and detects them correctly. So far, this system only covers all the letters of the English alphabet.
                 </p>
                 <div className="developer-info">
                     <h4>Developers</h4>
